8025321aa1b9bb7636e771f88f3d11c97dabef7a
rokgm <rok.grgicmesko@gmail.com>
2022-01-15 14:32:14 +0100

    Dodal za competition frisbee in gravity frisbee

--- stabilen_frisbee/simulacija.py
+++ stabilen_frisbee/simulacija.py
@@ -44,15 +44,13 @@ def plot_1_all(t, K, g, theta, C_dict, inital, inital_in_N=True):
     ax1.plot(t, sol[:, 1], label='$d_2$')
     ax1.plot(t, sol[:, 2], label='$v_1$')
     ax1.plot(t, sol[:, 3], label='$v_2$')
-    ax1.legend(loc='best')
     ax1.grid(linestyle='--')
-    ax1.legend(fancybox=False, edgecolor='black')
+    ax1.legend(fancybox=False, prop={'size':9})
 
     ax2.plot(N_sistem[:, 0], N_sistem[:, 1], label='(x, y), N sistem')
     ax2.plot(sol[:, 0], sol[:, 1], label='$(d_1, d_2)$, D sistem')
-    ax2.legend(loc='best')
     ax2.grid(linestyle='--')
-    ax2.legend(fancybox=False, edgecolor='black')
+    ax2.legend(fancybox=False, prop={'size':9})
     ax2.set_xlabel('x, d1 [m]')
     ax2.set_ylabel('y, d2 [m]')
     ax2.axis('equal')
@@ -60,23 +58,14 @@ def plot_1_all(t, K, g, theta, C_dict, inital, inital_in_N=True):
 
     ax3.plot(N_sistem[:, 2], N_sistem[:, 3], label='$(v_x, v_y)$, N sistem')
     ax3.plot(sol[:, 2], sol[:, 3], label='$(v_{d1}, v_{d2})$, D sistem')
-    ax3.legend(loc='best')
     ax3.grid(linestyle='--')
-    ax3.legend(fancybox=False, edgecolor='black')
+    ax3.legend(fancybox=False, prop={'size':9})
     ax3.set_xlabel('$v_x, v_{d1}$ [m/s]')
     ax3.set_ylabel('$v_y, v_{d2}$ [m/s]')
     plt.tight_layout()
     plt.show()
 
 def plot_N_trajectories(t, K, g, theta_list, C_list, inital, inital_in_N=True):
-    # R = []
-    # R0_0R = []
-    # C_L0, C_Lalpha, C_D0, C_Dalpha = [], [], [], []
-    # for i in range(len(theta_list)):
-    #     R[i] = np.array([[np.cos(theta_list[i]), -np.sin(theta_list[i])], [np.sin(theta_list[i]), np.cos(theta_list[i])]])
-    #     R0_0R[i] = np.block([[R[i], np.zeros((2,2))], [np.zeros((2,2)), R[i]]])   # diagonalna blocna
-    #     C_L0[i], C_Lalpha[i], C_D0[i], C_Dalpha[i] = C_list[i]['C_L0'], C_list[i]['C_Lalpha'], C_list[i]['C_D0'], C_list[i]['C_Dalpha']
-
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize = (6, 4))    
 
     for i in range(len(theta_list)):
@@ -95,9 +84,8 @@ def plot_N_trajectories(t, K, g, theta_list, C_list, inital, inital_in_N=True):
         N_sistem = frisbee_D_to_N(sol, R0_0R)
 
         ax.plot(N_sistem[:, 0], N_sistem[:, 1], label='C{}'. format(i + 1))
-        ax.legend(loc='best')
+        ax.legend(fancybox=False, prop={'size':9})
         ax.grid(linestyle='--')
-        ax.legend(fancybox=False, edgecolor='black')
         ax.set_xlabel('x [m]')
         ax.set_ylabel('y [m]')
         ax.axis('equal')
@@ -106,9 +94,15 @@ def plot_N_trajectories(t, K, g, theta_list, C_list, inital, inital_in_N=True):
     plt.tight_layout()
     plt.show()
 
-g = 9.8     
-m = 0.175
-A = 0.057
+g = 9.8
+
+m = 0.175 # competition disc
+# m = 0.027   # gravity disc
+
+# A = 0.0616 # competition disc
+A = 0.057 # iz clanka
+# A = 0.00312 # gravity disc
+
 ro = 1.23
 K = A * ro / (2 * m)
 
@@ -121,10 +115,10 @@ C4_dict = {'C_L0':-0.40, 'C_Lalpha':1.89, 'C_D0':0.83, 'C_Dalpha':0.83} # clanek
 C5_dict = {'C_L0':1.17, 'C_Lalpha':0.28, 'C_D0':5.07, 'C_Dalpha':0.077} # -||-
 
 t = np.linspace(0, 2, 101)
-theta = np.pi / 180 * 25       # za alpha blizu pi/2 je C_D vecji kot za disk
+theta = np.pi / 180 * 20
 
-# plot_1_all(t, K, g, theta, C2_dict, (0, 0, 15, -8))
+plot_1_all(t, K, g, theta, C2_dict, (0, 0, 15, -8))
 
-theta_list = np.ones(5) * np.pi / 180 * 20
+theta_list = np.ones(5) * theta
 C_list = [C1_dict, C2_dict, C3_dict, C4_dict, C5_dict]
 plot_N_trajectories(t, K, g, theta_list, C_list, (0, 0, 15, -8))
